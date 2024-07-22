8efbdfc1f9524499283a01473d2fb06af061c099
Rahul Kumar <rahulkumarsahu8182@gmail.com>
2024-07-22 18:45:28 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/React_Gemini_Clone_01/",
-  esolve: {
+  resolve: {
     alias: {
       "@": "/src",
     },
